c220d5202fb7c9bbe38b8d3ae5d3006e7637298b
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-08-29 18:46:52 +0200

    Compute the contour skin to the creature

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ struct MyWorldCoords(Vec2);
 struct Squeleton {
     nodes: Vec<(Vec3, f32)>,
     distance: f32,
+    skin: Vec<Vec2>,
 }
 
 impl Squeleton {
@@ -39,7 +40,7 @@ impl Squeleton {
             ));
         }
 
-        Squeleton { distance, nodes }
+        Squeleton { distance, nodes , skin:Vec::new()}
     }
 }
 
@@ -131,6 +132,10 @@ fn follow_anchor(mut squeletons: Query<&mut Squeleton>, mut gizmos: Gizmos) {
         let node_distance = squeleton.distance;
         let mut iter = squeleton.nodes.iter_mut().peekable();
 
+        let mut skin_left = Vec::<Vec2>::new();
+        let mut skin_right = Vec::<Vec2>::new();
+        let mut skin_head_tail = Vec::<Vec2>::new();
+
         loop {
             if let Some(head) = iter.next() {
                 if let Some(mut tail) = iter.peek_mut() {
@@ -153,8 +158,23 @@ fn follow_anchor(mut squeletons: Query<&mut Squeleton>, mut gizmos: Gizmos) {
                         ray.origin + *ray.direction * distance,
                         COLOR_WHITE,
                     );
-                    gizmos.circle(head.0, Dir3::Z, head.1, COLOR_WHITE);
-                    gizmos.circle(tail.0, Dir3::Z, tail.1, COLOR_WHITE);
+                    gizmos.circle_2d(head.0.truncate(), head.1, COLOR_WHITE);
+                    gizmos.circle_2d(tail.0.truncate(), tail.1, COLOR_WHITE);
+
+                    // Compute the skin points
+                    let front = ray.origin + -*ray.direction * head.1;
+                    let left = ray.origin + ray.direction.perp() * head.1;
+                    let right = ray.origin + -ray.direction.perp() * head.1;
+                    let back = ray.origin + ray.direction.perp() * head.1;
+
+                    skin_left.push(left);
+                    skin_right.push(right);
+                    skin_head_tail.push(front);
+                    skin_head_tail.push(back);
+
+                    gizmos.circle_2d(front, 5.0, COLOR_WHITE);
+                    gizmos.circle_2d(left, 5.0, COLOR_WHITE);
+                    gizmos.circle_2d(right, 5.0, COLOR_BLUE);
                 } else {
                     break;
                 }
@@ -162,6 +182,42 @@ fn follow_anchor(mut squeletons: Query<&mut Squeleton>, mut gizmos: Gizmos) {
                 break;
             }
         }
+
+        // Compute the last segment by looking at the 2 lasts nodes
+        let mut back_iter = squeleton.nodes.iter().rev().peekable();
+        if let Some(tail) = back_iter.next() {
+            if let Some(head) = back_iter.peek() {
+                let ray = Ray2d {
+                    origin: tail.0.truncate(),
+                    direction: Dir2::new_unchecked((tail.0 - head.0).truncate().normalize()),
+                };
+
+                // Compute the skin points
+                let left = ray.origin + ray.direction.perp() * tail.1;
+                let right = ray.origin + -ray.direction.perp() * tail.1;
+                let back = ray.origin + ray.direction * tail.1;
+
+                skin_left.push(left);
+                skin_right.push(right);
+                skin_head_tail.push(back);
+
+                gizmos.circle_2d(back, 5.0, COLOR_WHITE);
+                gizmos.circle_2d(left, 5.0, COLOR_WHITE);
+                gizmos.circle_2d(right, 5.0, COLOR_BLUE);
+            }
+        }
+
+        // Combine all the skin point
+        if let Some(tail) = skin_head_tail.last() {
+            skin_right.push(*tail);
+        }
+        skin_right.reverse();
+        // push the head front at the bottom of the reversed right, to make it at the top of the vec
+        if let Some(head) = skin_head_tail.first() {
+            skin_right.push(*head);
+        }
+        skin_left.append(&mut skin_right);
+        squeleton.skin = skin_left;
     }
 }
 
@@ -194,17 +250,33 @@ fn draw_body(
     //         head.0.y = bezier.position(t).y;
     //     }
 
-    let head = &squeleton.nodes[0].0;
-    let tail = &squeleton.nodes[1].1;
-    let direction = Ray2d {
-        origin: head.truncate(),
-        direction: Dir2::new_unchecked((*tail - *head).truncate().normalize()),
-    };
+    // let head = &squeleton.nodes[0].0;
+    // let tail = &squeleton.nodes[1].0;
+
+    // gizmos.line_2d(
+    //     direction.origin,
+    //     direction.origin + *direction.direction * squeleton.nodes[0].1,
+    //     COLOR_WHITE,
+    // );
 
     let mut points = Vec::<Vec2>::new();
-    for node in &squeleton.nodes {
-        points.push(node.0.truncate());
-    }
+    // for node in &squeleton.skin {
+        // points.push(node.0.truncate());
+
+        // let direction = Ray2d {
+        //     origin: head.truncate(),
+        //     direction: -Dir2::new_unchecked((*tail - *head).truncate().normalize()),
+        // };
+
+        // let front = direction.origin + *direction.direction * node.1;
+        // let left = direction.origin + direction.direction.perp() * node.1;
+        // let right = direction.origin + -direction.direction.perp() * node.1;
+        // gizmos.circle_2d(front, 5.0, COLOR_WHITE);
+        // gizmos.circle_2d(left, 5.0, COLOR_WHITE);
+        // gizmos.circle_2d(right, 5.0, COLOR_BLUE);
+    // }
+
+    let points = squeleton.skin.clone();
 
     let shape = shapes::Polygon {
         points: points.into_iter().collect(),
